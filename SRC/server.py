dd044fede419725e9736b401bd0865acb43fdfc0
JeroK1033 <jeronimohenao4@gmail.com>
2026-05-05 09:15:50 -0500

    Actualizacion2

--- SRC/server.py
+++ SRC/server.py
@@ -30,7 +30,7 @@ def analizar_residuo(imagen_bytes: bytes) -> dict:
     """
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash-lite",
+        model="gemini-3.1-flash-lite-preview",
         contents=[instruccion, img],
         config=types.GenerateContentConfig(
             response_mime_type="application/json",
